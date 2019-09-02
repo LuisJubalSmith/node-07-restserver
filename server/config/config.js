db86334a2427b9355c5bccf936f2e83e9b7f855c
luis <jubal8776@gmail.com>
2019-09-02 15:49:30 -0600

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,22 @@
 //  Puerto
 // ============================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ===========================
+// Entorno
+// ===========================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//=============================
+// Base de datos
+// ============================
+
+let urlDB;
+
+// if (process.env.NODE_ENV === 'dev') {
+//     urlDB = 'mongodb://localhost:27017/cafe';
+// } else {
+urlDB = 'mongodb+srv://smith8776:ian13246102!@cluster0-nf8mo.mongodb.net/cafe';
+// }
+process.env.URLDB = urlDB;
